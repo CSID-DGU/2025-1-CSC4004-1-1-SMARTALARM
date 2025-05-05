e3a5c7458151e4b07c209184acbc9e8daa1f793c
kjunsu010823 <kjunsu010823@gmail.com>
2025-05-05 17:02:10 +0900

    Feat: MainDashboard 및 AlarmSetupPage 구현 및 Expo 설정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import MainDashboard from './src/screens/MainDashboard';
+import AlarmSetupPage from './src/screens/AlarmSetupPage';  // 알람 셋업 페이지 추가
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <MainDashboard />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainDashboard">
+        <Stack.Screen name="MainDashboard" component={MainDashboard} />
+        <Stack.Screen name="AlarmSetupPage" component={AlarmSetupPage} />  {/* 연결 */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
